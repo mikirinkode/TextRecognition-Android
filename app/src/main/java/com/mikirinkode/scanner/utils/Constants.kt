bbe7fb418f9719d32397f92fd17bdd1a26981ff8
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-04-28 09:55:42 +0700

    move private field to local properties

---
+++ app/src/main/java/com/mikirinkode/scanner/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.mikirinkode.scanner.utils
+
+import com.mikirinkode.scanner.BuildConfig
+
+class Constants {
+    companion object {
+        const val PASSPHRASE = BuildConfig.PASSPHRASE
+        const val DB_NAME = BuildConfig.DB_NAME
+    }
+}
